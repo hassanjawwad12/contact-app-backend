64b7e57784c18761d1bf092db913825f10aec375
Muhammad Hassan Jawwad <hassanjawwad01@gmail.com>
2023-12-29 12:11:53 +0500

    jwt access token and login

--- Controller/userController.js
+++ Controller/userController.js
@@ -1,5 +1,6 @@
 const asyncHandler = require('express-async-handler');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const User = require('../Models/userModel');
 const { ConnectionStates } = require('mongoose');
 
@@ -46,13 +47,46 @@ const registerUser = asyncHandler(async (req, res) => {
    
 });
 
+
 //@desc Login a user
 //@route POST /api/users/login
 //@access Public
 const loginUser = asyncHandler(async (req, res) => {
-    res.json({message:"Login the User"})
+    const {email,password}=req.body;
+    if(!email || !password){
+        res.status(400);
+        throw new Error("Please provide all the fields");
+    }
+
+    const user = await User.findOne({email});
+    //compare the password with the hashed password
+    if(user && (await bcrypt.compare(password,user.password)))
+    {
+
+        const accessToken = jwt.sign(
+            {
+                user :{
+                    username:user.username,
+                    email:user.email,
+                    id:user._id,
+                }
+
+             }
+            ,process.env.ACCESS_TOKEN_SECRET,
+            {
+                expiresIn:"2m",
+            }
+             );
+        res.json({accessToken});
+
+    }
+    else{
+        res.status(401);
+        throw new Error("Invalid email or password");
+    }
    
 });
+
 
 //@desc Get a user information
 //@route GET /api/users/login
