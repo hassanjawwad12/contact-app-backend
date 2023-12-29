aac32a3c84d7b609dd443dc94567f8a63d68fe71
Muhammad Hassan Jawwad <hassanjawwad01@gmail.com>
2023-12-29 11:54:52 +0500

    User Registration and Password Hashing

--- Controller/userController.js
+++ Controller/userController.js
@@ -1,9 +1,47 @@
 const asyncHandler = require('express-async-handler');
+const bcrypt = require('bcrypt');
+const User = require('../Models/userModel');
+const { ConnectionStates } = require('mongoose');
 
 //@desc Register a user
 //@route POST /api/users/register
 //@access Public
 const registerUser = asyncHandler(async (req, res) => {
+    const {username,email,password}=req.body;
+    if(!username || !email || !password){
+        res.status(400);
+        throw new Error("Please provide all the fields");
+    }
+
+    const userAvailable= await User.findOne({email});
+    if(userAvailable){
+        res.status(400);
+        throw new Error("User already exists");
+    }
+
+    //we cannot store the raw password so we hash it using the bcrypt library and speed up the process 
+    //hash password ,  bcrypt provide us with a promise so we have to use await
+    const hashedPassword= await bcrypt.hash(password,10);
+    //10 is the number of salt rounds 
+   console.log("Hashed Password:",hashedPassword);
+
+   const user= await User.create({
+         username,
+         email,
+         password:hashedPassword,
+    });
+    console.log ("user created:",user);
+
+    if(user){
+        res.status(201).json({
+            _id:user._id,
+            email:user.email,
+        })
+    }
+    else {
+        res.status(400);
+        throw new Error("Invalid user data");
+    }
     res.json({message:"Register the User"})
    
 });
--- Models/userModel.js
+++ Models/userModel.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const userSchema = mongoose.Schema({
     
-        name: {
+        username: {
             type: String,
             required: [true, 'Please provide a name'],
         },
