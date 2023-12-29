aff9d5abc81aeb7e1caf65376d8129754dc877a1
Muhammad Hassan Jawwad <hassanjawwad01@gmail.com>
2023-12-29 11:32:55 +0500

    Added userSchema in Model

---
+++ Models/userModel.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const userSchema = mongoose.Schema({
+    
+        name: {
+            type: String,
+            required: [true, 'Please provide a name'],
+        },
+        email: {
+            type: String,
+            required: [true, 'Please provide an email'],
+            unique: true
+        },
+        password: {
+            type: String,
+            required: [true, 'Please provide a password'],
+        },
+       
+    },
+    {
+        timestamps: true
+    }
+);
+
+module.exports = mongoose.model('User', userSchema);
